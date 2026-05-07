70c2490be53e20f3020e8141b141624ea547daf4
Brad <ibanezbradley@gmail.com>
2026-05-07 22:51:05 +0800

    Practice Application Properties
    - Demonstrate how any of the environment properties you input into your application properties are retrieved and used to change any configuration in your spring boot application

--- src/main/java/com/iluv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/iluv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,15 +1,17 @@
 package com.iluv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
-    // expose "/" that returns "Hello WOrld"
+    @Value("${coach.name}")
+    private String coachName;
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World";
+        return coachName;
     }
 }
