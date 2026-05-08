ba22687e6cafdb4c16d9676d50490882a57f5246
Brad <ibanezbradley@gmail.com>
2026-05-08 20:32:51 +0800

    Remove @PostConstructor and @PreDestroy
    - to Demonstrate @Configuration and @Bean

--- src/main/java/com/iluv2code/springboot/demo/mycoolapp/common/CricketCoach.java
+++ src/main/java/com/iluv2code/springboot/demo/mycoolapp/common/CricketCoach.java
@@ -13,18 +13,6 @@ public class CricketCoach implements Coach {
         System.out.println("In Constructor: " + getClass().getSimpleName());
     }
 
-    //define init method
-
-    @PostConstruct
-    public void doStartupStuff() {
-        System.out.println("In DoMyStartupStuff " + getClass().getSimpleName());
-    }
-    //define destroy method
-    @PreDestroy
-    public void doCleanupStuff() {
-        System.out.println("In DoMyCleanupStuff " + getClass().getSimpleName());
-    }
-
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes :-)";
