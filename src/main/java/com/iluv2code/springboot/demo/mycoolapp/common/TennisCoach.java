344b5e712da4fc2500332dfaf4d81791e7c4a003
Brad <ibanezbradley@gmail.com>
2026-05-08 19:23:48 +0800

    Demonstrate @Lazy
    - On start-up, Spring creates an initialized object of each class, when you added @Lazy annotation under the class, this allows for it to not initialize up until it is needed or explicitly used. This can save memory

--- src/main/java/com/iluv2code/springboot/demo/mycoolapp/common/TennisCoach.java
+++ src/main/java/com/iluv2code/springboot/demo/mycoolapp/common/TennisCoach.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class TennisCoach implements Coach {
+
+    public TennisCoach () {
+        System.out.println("In Constructor: " + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
--- src/main/java/com/iluv2code/springboot/demo/mycoolapp/common/TrackCoach.java
+++ src/main/java/com/iluv2code/springboot/demo/mycoolapp/common/TrackCoach.java
@@ -1,11 +1,16 @@
 package com.iluv2code.springboot.demo.mycoolapp.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@Lazy
 public class TrackCoach implements Coach {
+
+    public TrackCoach () {
+        System.out.println("In Constructor: " + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
