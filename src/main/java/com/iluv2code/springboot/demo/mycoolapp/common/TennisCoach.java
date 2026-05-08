e2f64bbda4b3c24d4fbcf395cb8a5ef9e13546e9
Brad <ibanezbradley@gmail.com>
2026-05-08 18:22:47 +0800

    Added Extra Implementations
    - Added more class to the interface, to demonstrate how @Qualifier works, though at this stage of the commit, Nothing works because Spring is confused which implementation should it inject to my controller

---
+++ src/main/java/com/iluv2code/springboot/demo/mycoolapp/common/TennisCoach.java
@@ -0,0 +1,12 @@
+package com.iluv2code.springboot.demo.mycoolapp.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+}
