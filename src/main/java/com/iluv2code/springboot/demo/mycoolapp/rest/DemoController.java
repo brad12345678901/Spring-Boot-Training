5b76215d082912e3fb20232284b405e05abde40e
Brad <ibanezbradley@gmail.com>
2026-05-08 19:52:34 +0800

    Check Bean Scope
    - By default, your initialization of beans are set to singleton, what this means is similar to like primative data types, if another declared dependency uses the same abstract interface and you did dependency injection on it, it will point to the same instance of the injected bean.
    
    - myCoach ---> cricketCoach() <---- anotherCoach

--- src/main/java/com/iluv2code/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/iluv2code/springboot/demo/mycoolapp/rest/DemoController.java
@@ -12,14 +12,23 @@ public class DemoController {
     // define a private field of the dependency
     private Coach myCoach;
 
+    private Coach anotherCoach;
+
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach,
+    @Qualifier("cricketCoach") Coach theAnotherCoach){
         System.out.println("In Constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Company beans: myCoach === anotherCoach: " + (myCoach == anotherCoach);
     }
 }
