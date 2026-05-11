1b17bf7c06ea9bb1a29ef259b6783a346739cf62
Brad <ibanezbradley@gmail.com>
2026-05-12 01:10:22 +0800

    Implement findById Method
    - Added code for finding specific employee

--- src/main/java/com/iluv2code/springboot/demo/mycoolapp/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/iluv2code/springboot/demo/mycoolapp/dao/EmployeeDAOJpaImpl.java
@@ -19,12 +19,28 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
     public EmployeeDAOJpaImpl (EntityManager entityManager) {
         this.entityManager = entityManager;
     }
+
+    @Override
     public List<Employee> findAll() {
-        // create query
         TypedQuery<Employee> theQuery = entityManager.createQuery("from Employee", Employee.class);
-        // execute the query and result list
         List<Employee> employees = theQuery.getResultList();
-        // return the list of employees
         return employees;
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        //get employee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
     }
 }
