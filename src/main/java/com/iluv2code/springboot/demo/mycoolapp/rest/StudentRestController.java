b825c0ec93da7df3d668e95d758744d51432e02b
Brad <ibanezbradley@gmail.com>
2026-05-11 19:26:35 +0800

    Added PostConstruct
    - So you only have to create one in-memory database instead of creating a new one per call which can hurt performance

--- src/main/java/com/iluv2code/springboot/demo/mycoolapp/rest/StudentRestController.java
+++ src/main/java/com/iluv2code/springboot/demo/mycoolapp/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.iluv2code.springboot.demo.mycoolapp.rest;
 
 import com.iluv2code.springboot.demo.mycoolapp.entity.StudentJackson;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +13,21 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoints for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<StudentJackson> getStudents() {
-        List<StudentJackson> theStudents = new ArrayList<>();
+    private List<StudentJackson> theStudents;
 
+    // define @PostConstruct to load the student data ... only once!
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
         theStudents.add(new StudentJackson("Poornima", "Patel"));
         theStudents.add(new StudentJackson("Mario", "Rossy"));
         theStudents.add(new StudentJackson("Mary", "Smith"));
+    }
+
+    // define endpoints for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<StudentJackson> getStudents() {
         return theStudents;
     }
 }
