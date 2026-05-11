4ad8ec29e3f3bad51e2e0ab63ba31850afa6bfa1
Brad <ibanezbradley@gmail.com>
2026-05-12 01:29:03 +0800

    Implement Finding Specific Employee API
    - Expanded the service interface to have more methods for getting a specific employee, saving an employee, updating an employee and deleting employee
    - Implement get specific employee, create GetMapping with "employees/{employeeId}", the curly braces represent the id itself, then you add @PathVariable into your parameter that must be the same name as the one in the Mapping to get the parameter and return your specific employee

--- src/main/java/com/iluv2code/springboot/demo/mycoolapp/rest/EmployeeRestController.java
+++ src/main/java/com/iluv2code/springboot/demo/mycoolapp/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.iluv2code.springboot.demo.mycoolapp.dao.EmployeeDAO;
 import com.iluv2code.springboot.demo.mycoolapp.entity.Employee;
 import com.iluv2code.springboot.demo.mycoolapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,5 +28,16 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    // add mapping for GET "/employees/{employeezid}"
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null)
+            throw new RuntimeException("Employee ID not found - "+employeeId);
+
+        return theEmployee;
     }
 }
