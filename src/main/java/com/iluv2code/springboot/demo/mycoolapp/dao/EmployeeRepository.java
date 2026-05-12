acfc98af45ed32fe3ca14496a0cdbaf84f095db2
Brad <ibanezbradley@gmail.com>
2026-05-12 20:38:33 +0800

    Edit Page Size
    - to reduce the amount of rows to show per page

--- src/main/java/com/iluv2code/springboot/demo/mycoolapp/dao/EmployeeRepository.java
+++ src/main/java/com/iluv2code/springboot/demo/mycoolapp/dao/EmployeeRepository.java
@@ -2,9 +2,7 @@ package com.iluv2code.springboot.demo.mycoolapp.dao;
 
 import com.iluv2code.springboot.demo.mycoolapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it, no need to code ;)
